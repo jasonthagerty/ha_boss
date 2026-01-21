d0a188605ab56a837dd95eb9c3336253059590cc
Jason Hagerty <jasonthagerty@gmail.com>
2026-01-21 13:49:05 -0600

    fix for issue #173

--- ha_boss/core/migrations/__init__.py
+++ ha_boss/core/migrations/__init__.py
@@ -117,7 +117,9 @@ class MigrationRegistry:
 MIGRATION_REGISTRY = MigrationRegistry()
 
 
-def register_migration(target_version: int, description: str) -> Callable[
+def register_migration(
+    target_version: int, description: str
+) -> Callable[
     [Callable[[AsyncSession], Coroutine[Any, Any, None]]],
     Callable[[AsyncSession], Coroutine[Any, Any, None]],
 ]:
